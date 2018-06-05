6b76b7a7dabe61daba5bf78c4c30b78c59aa1e35
Gabriel Valle <gvalle91@gmail.com>
2018-06-05 16:29:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,22 @@ const codes = [
   "b",
   "a"
 ];
-
+let index = 0;
+function onKeyDownHandler(e){
+  const key = e.key;
+  if (key === codes[index]){
+    index++;
+    if (index === codes.length){
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(e){
+    onKeyDownHandler(e);
+  });
 }
